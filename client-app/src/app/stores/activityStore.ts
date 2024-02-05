a045d5af52762d9d2ee3ec180d348dd05aad9784
Mihail Sevagin <lolvillian@gmail.com>
2024-02-05 23:57:08 +0200

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -14,8 +14,18 @@ export default class ActivityStore {
         makeAutoObservable(this);
     }
 
-    get activitiesByDate(){
+    get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as { [key: string]: Activity[] })
+        )
     }
 
     loadActivities = async () => {
@@ -40,7 +50,7 @@ export default class ActivityStore {
             this.selectedActivity = activity;
             return activity;
         }
-        else{
+        else {
             this.setLoadingInitial(true);
             try {
                 activity = await agent.Activities.details(id);
@@ -105,7 +115,7 @@ export default class ActivityStore {
                 this.activityRegistry.delete(id);
                 this.loading = false;
             })
-        } catch (error){
+        } catch (error) {
             console.log(error);
             runInAction(() => this.loading = false);
         }
