7263d546fe0c59dd26b6c5fae0449304464b5630
Mihail Sevagin <lolvillian@gmail.com>
2024-03-31 20:03:02 +0300

    End of section 15

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,9 +1,65 @@
-export interface Activity {
-    id: string
-    title: string
-    date: Date | null
-    description: string
-    category: string
-    city: string
-    venue: string
+import { Profile } from "./profile"
+
+export interface IActivity {
+  id: string;
+  title: string;
+  date: Date | null;
+  description: string;
+  category: string;
+  city: string;
+  venue: string;
+  isHost?: boolean;
+  isGoing?: boolean;
+  isCancelled?: boolean;
+  hostUsername?: string;
+  host?: Profile;
+  attendees?: Profile[];
+}
+
+export class Activity implements IActivity {
+  id: string;
+  title: string;
+  date: Date | null;
+  description: string;
+  category: string;
+  city: string;
+  venue: string;
+  isHost: boolean = false;
+  isGoing: boolean = false;
+  isCancelled: boolean = false;
+  hostUsername: string = '';
+  host?: Profile;
+  attendees?: Profile[];
+
+  constructor(init: ActivityFormValues){
+    this.id = init.id!;
+    this.title = init.title;
+    this.description = init.description;
+    this.category = init.category;
+    this.date = init.date;
+    this.city = init.city;
+    this.venue = init.venue;
   }
+}
+
+export class ActivityFormValues {
+  id?: string = undefined;
+  title: string = '';
+  date: Date | null = null;
+  description: string = '';
+  category: string = '';
+  city: string = '';
+  venue: string = '';
+
+  constructor(activity?: ActivityFormValues) {
+    if(activity){
+      this.id = activity.id;
+      this.title = activity.title;
+      this.description = activity.description;
+      this.category = activity.category;
+      this.date = activity.date;
+      this.city = activity.city;
+      this.venue = activity.venue;
+    }
+  }
+}
