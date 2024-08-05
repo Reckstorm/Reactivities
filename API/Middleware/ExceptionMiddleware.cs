85cb0b7ec0530d3a9536f0143d7416dbc3bc003c
Administrator <gitlab_admin_d5ab0c@example.com>
2024-08-06 00:36:47 +0300

    Reverted changes made to middleware to catch the error on production

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -31,7 +31,7 @@ public class ExceptionMiddleware
 
             var response = _env.IsDevelopment()
                 ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace)
-                : new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace);
+                : new AppException(context.Response.StatusCode, ex.Message);
 
             var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
             var json = JsonSerializer.Serialize(response, options);
